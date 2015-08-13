927c0ffdcb5db3c3cdd7c93ebac4ed3052649d43
Blake Bender <blakeben@microsoft.com>
2015-08-13 14:30:34 -0700

    Add license text to newly added files, and Doxygen comments explaining the purpose of the ToString override.

--- FBWinSDK/FBWinSDK/FBWinSDK.Shared/FacebookPermissions.h
+++ FBWinSDK/FBWinSDK/FBWinSDK.Shared/FacebookPermissions.h
@@ -1,19 +1,40 @@
+//******************************************************************************
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
 #pragma once
 
 namespace Facebook
 {
-	public ref class FBPermissions sealed
+    /*!\brief Simple class to contain a set of permissions for FB.  
+    */
+    public ref class FBPermissions sealed
 	{
 		public:
+            //! Construct an instance with a given set of permissions
             FBPermissions(
                 Windows::Foundation::Collections::IVectorView<Platform::String^>^ Permissions
                 );
 
+            //! The list of permissions
 			property Windows::Foundation::Collections::IVectorView<Platform::String^>^ Values
 			{
 				Windows::Foundation::Collections::IVectorView<Platform::String^>^ get();
 			};
 
+            //! Handy override that provides a string version of permissions in
+            // the format suitable for concatenating into a FB request URL.
 			virtual Platform::String^ ToString(
 				) override;
 
