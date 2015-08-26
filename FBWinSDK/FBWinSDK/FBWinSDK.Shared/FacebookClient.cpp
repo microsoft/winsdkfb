d710d1f2d4c76346d5568188f46eb967e1be7269
Austin Diviness <austdi@microsoft.com>
2015-08-26 13:50:51 -0700

    request_host removes ability to specify api version

--- FBWinSDK/FBWinSDK/FBWinSDK.Shared/FacebookClient.cpp
+++ FBWinSDK/FBWinSDK/FBWinSDK.Shared/FacebookClient.cpp
@@ -610,6 +610,7 @@ Uri^ FBClient::PrepareRequestUri(
     }
 
     String^ host;
+    String^ apiVersion = L"";
 
     if (parametersWithoutMediaObjects->HasKey("request_host"))
     {
@@ -619,13 +620,11 @@ Uri^ FBClient::PrepareRequestUri(
     else
     {
         host = L"graph.facebook.com";
+        if (sess->APIMajorVersion)
+        {
+            apiVersion = L"v" + sess->APIMajorVersion.ToString() + L"." + sess->APIMinorVersion.ToString() + L"/";
+        }
     }
-
-	String^ apiVersion = L"";
-	if (sess->APIMajorVersion) 
-	{
-		apiVersion = L"v" + sess->APIMajorVersion.ToString() + L"." + sess->APIMinorVersion.ToString() + L"/";
-	}
 
     String^ uriString = L"https://" + host + L"/" + apiVersion + path + L"?" + queryString;
 
