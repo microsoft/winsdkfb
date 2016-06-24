ad9ceb180830d5f594757450d671a0e7485f3e84
Austin Diviness <austdi@microsoft.com>
2016-06-24 12:54:34 -0700

    added better gitignore, changed some tabs to spaces

--- _includes/SampleCode/SampleCode/MainPage.xaml.h
+++ _includes/SampleCode/SampleCode/MainPage.xaml.h
@@ -9,13 +9,13 @@
 
 namespace SampleCode
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
-	public ref class MainPage sealed
-	{
-	public:
-		MainPage();
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public ref class MainPage sealed
+    {
+    public:
+        MainPage();
 
-	};
+    };
 }
