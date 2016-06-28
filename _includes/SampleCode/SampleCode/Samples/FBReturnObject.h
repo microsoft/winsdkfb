9f22254b024b36ff5d842dbf3513fabe512152cd
Austin Diviness <austdi@microsoft.com>
2016-06-28 06:13:24 -0700

    cpp video upload sample builds correctly

--- _includes/SampleCode/SampleCode/Samples/FBReturnObject.h
+++ _includes/SampleCode/SampleCode/Samples/FBReturnObject.h
@@ -16,7 +16,21 @@ namespace SampleCode
                 _id = value;
             }
         }
+
+        property Platform::String^ Post_Id
+        {
+            Platform::String^ get()
+            {
+                return _post_id;
+            }
+            void set(Platform::String^ value)
+            {
+                _post_id = value;
+            }
+        }
+
     private:
         Platform::String^ _id;
+        Platform::String^ _post_id;
     };
 }
